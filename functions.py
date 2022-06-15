8b852d62c4e8d556d498a7a30f9abda499925ada
Bipul Chaudhary <bipulchau@gmail.com>
2022-06-15 10:03:47 -0400

    Function examples

---
+++ functions.py
@@ -0,0 +1,22 @@
+def greet():
+    print("Hello There")
+
+
+greet()
+print(greet())
+
+
+def addition(x, y):
+    value = x + y
+    return value
+
+
+print(addition(9, 2.5))
+a = addition(2, 3)
+
+print(a)
+print(addition(addition(9, 1), addition(2, 3)))
+
+
+
+
