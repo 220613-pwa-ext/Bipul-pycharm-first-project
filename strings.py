b4a5de6ee5e2ba6c3bcdf10058e5bcb96bc3d980
Bipul Chaudhary <bipulchau@gmail.com>
2022-06-15 21:55:57 -0400

    String slicing

---
+++ strings.py
@@ -0,0 +1,17 @@
+name = "Bipul Chaudhary"
+address = "Toronto Canada"
+
+print(name)
+print(address)
+
+# slicing
+print(name[0])
+print(name[-1])
+print(name[0:7])
+print(name[::])
+print(name[::-1])
+print(name[::2])
+print(name[-8:-1])
+print(name[-9:])
+print(name[-9:-1])
+
