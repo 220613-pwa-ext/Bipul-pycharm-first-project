be9756c5d3c4371f721ccd1ab82e4b532ad7a99a
Bipul Chaudhary <bipulchau@gmail.com>
2022-06-15 22:24:27 -0400

    comparsion and logical operator example

---
+++ comparison-and-logical-operators.py
@@ -0,0 +1,18 @@
+# comparisons
+print(3 == 3)
+print("dog" == "dog")
+print("dog" == "Dog")
+print("dog" != "Dog")
+print(3 == 3.0)
+
+# returns address of an object/identifier
+print(id("dog"))
+animal = "dog"
+print(id(animal))
+print("dog" is animal)
+print(3 is 3.0)
+
+age = 33
+
+print(not age == 34)
+print(not False)
