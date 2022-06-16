dc4eb40da836cbbefdec4320a15ad4cb63674f94
Bipul Chaudhary <bipulchau@gmail.com>
2022-06-15 22:09:30 -0400

    String methods

--- strings.py
+++ strings.py
@@ -15,3 +15,16 @@ print(name[-8:-1])
 print(name[-9:])
 print(name[-9:-1])
 
+
+print(name.upper())
+print(name.lower())
+dob = int(input("Year of birth: "))
+age = 2022 - dob
+print(f"{name} was born in {dob}. So he is now {age}")
+name2 = name.replace("B", "T")
+print(name2)
+name3 = "             Madhu"
+print(name3.strip())
+print(len(name3.strip()))
+print(name3.count("M"))
+print(name.find('a'))
